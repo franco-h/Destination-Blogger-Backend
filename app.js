3531eddd7879af2cb2ee2c9333dfc92b71a00ed3
Franco-H <francohui@berkeley.edu>
2022-06-15 14:41:11 -0700

    New update

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ let express = require('express');
 let cors = require('cors');
 // Import Post Schema
 let Post = require('./backend_model/post.model').Post;
+let uniqid = require("uniqid");
 let app = express();
+
+console.log(uniqid());
 
 app.use(cors());
 
@@ -14,6 +17,7 @@ app.use(express.json());
 // Generate unique id order for each JSON object
 let id = 1;
 
+/* ALL POST METHODS*/
 app.get('/posts', async(req, res) => {
     let posts = await Post.find();
     res.send(posts);
@@ -22,7 +26,7 @@ app.get('/posts', async(req, res) => {
 app.post('/posts', async(req, res) => {
     let reqBody = req.body;
     let newArticle = new Post({
-        id: '' + id++,
+        id: uniqid(),
         destination: reqBody.destination,
         location: reqBody.location,
         image: reqBody.image,
@@ -31,6 +35,12 @@ app.post('/posts', async(req, res) => {
     await newArticle.save();
     res.send("Created");
 });
+
+app.delete('/posts/:id',async (req, res) =>{
+    let id = req.params.id;
+    await Post.deleteOne({id:id});
+    res.send("Deleted");
+})
 
 app.use(express.static('client'));
 
