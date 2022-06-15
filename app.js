a55e3c33496b144b35c10f2088cd40834783e7c7
Franco-H <francohui@berkeley.edu>
2022-06-15 11:10:24 -0700

    Create article function at backend

--- app.js
+++ app.js
@@ -10,12 +10,27 @@ app.use(cors());
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://admin:nelly12345@travel.qxbuv8f.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true});
 
+app.use(express.json());
+// Generate unique id order for each JSON object
+let id = 1;
+
 app.get('/posts', async(req, res) => {
     let posts = await Post.find();
     res.send(posts);
 });
 
-
+app.get('/posts', async(req, res) => {
+    let reqBody = req.body;
+    let newArticle = new Post({
+        id: '' + id++,
+        destination: reqBody.destination,
+        location: reqBody.location,
+        image: reqBody.image,
+        description: reqBody.description
+    });
+    await newArticle.save();
+    res.send("Created");
+});
 
 app.use(express.static('client'));
 
